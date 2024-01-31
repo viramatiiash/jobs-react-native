de2570ab7e2279edaf6cba9387e14c73b57f38e9
Vira <virko1996@gmail.com>
2024-01-31 14:01:44 +0200

    initial commit

---
+++ infos.jsx
@@ -0,0 +1,10 @@
+// npx create-expo-app@latest -e with-router ./
+// npm install expo-font axios react-native-dotenv
+// npm install -g expo-cli
+// expo-cli start --tunnel
+// @expo/ngrok@^4.1.0
+
+// ! deploying
+// npm install -g expo-cli
+// expo publish
+// npm install -g expo-cli --force
